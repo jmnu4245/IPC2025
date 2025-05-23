330c146c37c5057dbfdfbaaf16c9f23e53087f62
Torrente <torrentep18@gmail.com>
2025-05-23 19:04:51 +0200

    css adaptado

--- src/controller/FXMLUserOptionsController.java
+++ src/controller/FXMLUserOptionsController.java
@@ -6,7 +6,10 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 
 /**
  * FXML Controller class
@@ -15,12 +18,17 @@ import javafx.fxml.Initializable;
  */
 public class FXMLUserOptionsController implements Initializable {
 
+    @FXML
+    private ImageView avatarIV;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        double radius = avatarIV.getFitWidth() / 2;
+        Circle circle = new Circle(radius, radius, radius);
+        avatarIV.setClip(circle);
     }    
     
 }
