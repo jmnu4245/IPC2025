31146057d20e1ffd744a33676ee05e438b461617
Torrente <torrentep18@gmail.com>
2025-05-26 18:16:05 +0200

    avances

--- src/controller/FXMLUserOptionsController.java
+++ src/controller/FXMLUserOptionsController.java
@@ -6,9 +6,15 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
 /**
@@ -20,15 +26,50 @@ public class FXMLUserOptionsController implements Initializable {
 
     @FXML
     private ImageView avatarIV;
-
+    @FXML
+    private TextField userField;
+    @FXML
+    private HBox emailError;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private Label dateError;
+    @FXML
+    private DatePicker dateField;
+    @FXML
+    private PasswordField passField;
+    @FXML
+    private Label passError;
+    
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        double radius = avatarIV.getFitWidth() / 2;
-        Circle circle = new Circle(radius, radius, radius);
-        avatarIV.setClip(circle);
+    public void initialize(URL url, ResourceBundle rb) {   
+    double width = avatarIV.getFitWidth();
+    double height = avatarIV.getFitHeight();
+    double radius = Math.min(width, height) / 2;
+   
+    // Centrar el círculo en el ImageView
+    Circle circle = new Circle(width / 2, height / 2, radius);
+    avatarIV.setClip(circle);
+
+        
     }    
+
+    @FXML
+    private void onActionAvatar(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void onActionCancell(ActionEvent event) {
+        emailField.getScene().getWindow().hide();
+    }
+
+    @FXML
+    private void onActionSave(ActionEvent event) {
+        
+    }
     
 }
