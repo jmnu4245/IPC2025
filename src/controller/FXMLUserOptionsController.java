a6f6c8d48f0bea2b252283bb14686f192f88d1d5
Torrente <torrentep18@gmail.com>
2025-05-26 19:11:39 +0200

    novabien

--- src/controller/FXMLUserOptionsController.java
+++ src/controller/FXMLUserOptionsController.java
@@ -5,17 +5,24 @@
 package controller;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
+import model.Navigation;
+import model.NavDAOException;
+
+import model.User;
 
 /**
  * FXML Controller class
@@ -23,7 +30,10 @@ import javafx.scene.shape.Circle;
  * @author pablo
  */
 public class FXMLUserOptionsController implements Initializable {
-
+    Image imageDef = new Image("resources/avatar_defecto.jpg");
+    User usuarioSesion = new User("José","jose2@upv.alumno.es","hoLa123!",imageDef,LocalDate.now());
+    
+    NavigationDAO dao = Navigation.getInstance().getDAO();
     @FXML
     private ImageView avatarIV;
     @FXML
@@ -45,7 +55,7 @@ public class FXMLUserOptionsController implements Initializable {
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {   
+    public void initialize(URL url, ResourceBundle rb) { 
     double width = avatarIV.getFitWidth();
     double height = avatarIV.getFitHeight();
     double radius = Math.min(width, height) / 2;
@@ -53,8 +63,22 @@ public class FXMLUserOptionsController implements Initializable {
     // Centrar el círculo en el ImageView
     Circle circle = new Circle(width / 2, height / 2, radius);
     avatarIV.setClip(circle);
-
-        
+    
+    dateField.setDayCellFactory((DatePicker picker) -> {
+    return new DateCell() {
+        @Override
+        public void updateItem(LocalDate date, boolean empty) {
+            super.updateItem(date, empty);
+            LocalDate today = LocalDate.now();
+            setDisable(empty || !date.isBefore(today)); // Deshabilita hoy y después
+            }
+        };
+    });
+    
+    userField.setText(usuarioSesion.getNickName());
+    emailField.setText(usuarioSesion.getEmail());
+    dateField.setValue(usuarioSesion.getBirthdate());
+    passField.setText(usuarioSesion.getPassword());
     }    
 
     @FXML
@@ -69,7 +93,16 @@ public class FXMLUserOptionsController implements Initializable {
 
     @FXML
     private void onActionSave(ActionEvent event) {
+        String newEmail = emailField.getText();
+        LocalDate newDate = dateField.getValue();
+        String newPass = passField.getText();
         
+        
+        
+    }
+    
+    public void setUsuarioSesion(User u){
+        usuarioSesion = u;
     }
     
 }
